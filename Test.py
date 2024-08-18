7ba8ec56a4ea06c895abab2f543343ba4e4e9edd
Kaspars Mačukāns <kasparsmp@gmail.com>
2024-08-18 23:42:12 +0300

    Changed everything

--- Test.py
+++ Test.py
@@ -1,8 +1,12 @@
-greeting = 'Hello World!'
-print(greeting)
+menu = "Latte, Black Coffee, Americano, Cappucino" #This is variable for menu
 
-name = "Kaspars"
-surname = "Mačukāns-Pētersons"
-ns = name + " " + surname
+print("Hello!")
+customer = input("What is your name?\n") #Creating variable from user input
 
-print(ns)
+print("Thank you " + customer + " for comming to our CoffeeShop!\n")
+
+order = input("Here is our menu:\n" + menu + "\n" + "What would you like to order?\n")
+
+#order = input()
+
+print("Sounds good "+ customer + ", your " + order + " will be ready in a moment.")

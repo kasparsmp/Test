c25387b97965fdec8212816d6e8511a486d97102
Kaspars Mačukāns <kasparsmp@gmail.com>
2024-08-20 17:49:09 +0300

    Camping

---
+++ Camping.py
@@ -0,0 +1,12 @@
+
+#Camping item list in variable
+camping_list = ["tent", "sleeping bag", "shoes", "flashlight", "water bottle", "water", "milk", "food"]
+
+camp_site = ["Līgatne", 404, 24.50, True]
+print(type(camping_list))
+print(type(camp_site))
+
+me = camping_list[0]
+you = camping_list[1]
+
+print("I will bring " + me + " and " + "you will bring " + you)

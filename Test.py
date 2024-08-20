9b52c401a7d05ec30d9d23a7dfa3d8d366e470dd
Kaspars Mačukāns <kasparsmp@gmail.com>
2024-08-20 17:50:16 +0300

    Yes

--- Test.py
+++ Test.py
@@ -1,16 +1,23 @@
 menu = "Latte, Black Coffee, Americano, Cappucino" #This is variable for menu
 
-name = input("Hello!What is your name?\n") #Creating variable from user input
+#Create variable for name
+name = input("Hello! What is your name?\n")
 
-if name == "Janis":
-    print("You are not wellcome here!!")
-    exit()
+#This is variable for name, and check if it is Janis or Juris, and  check if they are evil
+if name == "Janis" or name == "Juris" or name == "Loki":
+    status = input("You are evil?\n")
+    deeds = int(input("How many evil deeds do you do today?\n"))
+    if status == "yes" and deeds >= 4:
+        print(name + " you did " + str(deeds) + " deeds" + ", you are not wellcome here!!")
+        exit()
 else:
     print("Thank you " + name + " for comming to our CoffeeShop!\n")
 
 #This is variable for order
 order = input("Here is our menu:\n" + menu + "\n" + "What would you like to order?\n")
 
+#This is variable for price and check if milk is needed for Latte. If milk is needed, price is increased.
+#If user writes item that is not in menu, program will exit.
 if order == "Latte":
     milk = input("Would you like extra milk for your Latte?\n")
     if milk == "yes":
@@ -33,5 +40,5 @@ cups = input("How many cups would you like?\n")
 #This is variable for amount
 amount = price * int(cups)
 
-
+#Prints out text with confiramtion of order and payment amount
 print("Sounds good "+ name + ", your " + order + " will be ready in a moment." + "Your bill is " + str(amount) + "€")

466ed6a4dff1fb56e8d9145e5b4e6f50ea9977ac
Kaspars Mačukāns <kasparsmp@gmail.com>
2024-08-20 13:31:55 +0300

    If, else, elif funcions for coffee price

---
+++ 3_test.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name:\n")
+
+if name == "Kaspars":
+    print("Are you evil?")
+else:
+    print("Oh great, you are not evil")
+
--- Test.py
+++ Test.py
@@ -1,15 +1,37 @@
 menu = "Latte, Black Coffee, Americano, Cappucino" #This is variable for menu
-price = 5 #This is variable for price (all menu items have the same price)
 
+name = input("Hello!What is your name?\n") #Creating variable from user input
 
-print("Hello!")
-customer = input("What is your name?\n") #Creating variable from user input
+if name == "Janis":
+    print("You are not wellcome here!!")
+    exit()
+else:
+    print("Thank you " + name + " for comming to our CoffeeShop!\n")
 
-print("Thank you " + customer + " for comming to our CoffeeShop!\n")
-
+#This is variable for order
 order = input("Here is our menu:\n" + menu + "\n" + "What would you like to order?\n")
 
-cups = input("How many cups would you like?\n")
-amount = price * int(cups) #This is variable for amount
+if order == "Latte":
+    milk = input("Would you like extra milk for your Latte?\n")
+    if milk == "yes":
+        price = 9
+    else:
+        price = 8
+elif order == "Black Coffee":
+    price = 4
+elif order == "Americano":
+    price = 3
+elif order == "Cappucino":
+    price = 5
+else:
+    print("Sorry we don't have that item")
+    exit()
 
-print("Sounds good "+ customer + ", your " + order + " will be ready in a moment." + "Your bill is " + str(amount) + "€")
+#This is variable for cups
+cups = input("How many cups would you like?\n")
+
+#This is variable for amount
+amount = price * int(cups)
+
+
+print("Sounds good "+ name + ", your " + order + " will be ready in a moment." + "Your bill is " + str(amount) + "€")
